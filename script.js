3a2551ce63e0e47e728b0ae623c4e1d2480a4f62
micrastur <marina.lisovik@gmail.com>
2016-12-12 22:27:03 +0200

    Activated game by clicking on elements

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ class Game {
         data.cells = data.defaultSize[0] * data.defaultSize[1];
         data.content = this.mixData(data.cells);
 
-        this.getData = function () {
+        this.getData = () => {
             let dataCopy = data;
             return dataCopy;
         };
@@ -25,12 +25,11 @@ class Game {
             gameElement = gameData.blockElement,
             gameContent = gameData.content;
 
-        console.log(gameData.content);
 
         document.getElementsByTagName('h1')[0].innerHTML = gameData.name;
         this.buildTable(gameElement, gameData.defaultSize);
         this.fillContent(gameContent);
-
+        this.activateGame();
     }
 
     buildTable(element, size) {
@@ -40,9 +39,9 @@ class Game {
 
         for (let i = 0; i < row; i++) {
             let item = i + 1;
-            str += "<div class='row row-" + item + "'>";
+            str += "<div class='row'>";
             for (let k = 0; k < col; k++) {
-                str += "<div></div>"
+                str += "<div class='cell'></div>";
             }
             str += "</div>";
         }
@@ -77,18 +76,65 @@ class Game {
         }
 
         console.log(comparableItems);
+        console.log(numbers);
         if(comparableItems % 2 !== 0){
-            let n = numbers[1];
-            numbers[1] = numbers[2];
-            numbers[2] = n;
+            let n = numbers[0];
+            numbers[0] = numbers[1];
+            numbers[1] = n;
         }
+        console.log(numbers);
         return numbers;
     }
     fillContent(content) {
-        let cellsElement = document.querySelectorAll('.row div');
+        let cellsElement = document.querySelectorAll('.cell');
         for (let i = 0, len = content.length; i < len; i++){
-            cellsElement[i].innerHTML = content[i];
+            let contentItem = content[i],
+                currentElement = cellsElement[i];
+
+            if (typeof contentItem !== "number") {
+                currentElement.id = "empty"
+            }
+            currentElement.innerHTML = contentItem;
         }
+    }
+    activateGame() {
+        let _self = this;
+        document.onkeydown =  function (event) {
+            let keyName = event.code.toLocaleLowerCase().replace('arrow', '');
+            _self.move(null, keyName);
+        };
+        document.getElementById('game').onclick = function (event) {
+            if(event.target !== this && !event.target.id){
+                _self.move(event.target);
+            }
+        }
+    }
+    move(element, keyDirection) {
+        let cellElements = Array.prototype.slice.call(document.getElementsByClassName('cell')),
+            emptyElement = document.getElementById('empty'),
+            emptyIndex = cellElements.indexOf(emptyElement),
+            movements = {
+                left: element.previousSibling,
+                top: cellElements[cellElements.indexOf(element)-4],
+                right: element.nextSibling,
+                bottom: cellElements[cellElements.indexOf(element)+4]
+            };
+
+        if (!element){
+
+        } else {
+            for (let key in movements){
+                if(movements.hasOwnProperty(key) && movements[key] === emptyElement){
+                    let currentValue = element.innerHTML;
+                    element.innerHTML = emptyElement.innerHTML;
+                    emptyElement.innerHTML = currentValue;
+
+                    element.setAttribute('id', 'empty');
+                    emptyElement.removeAttribute('id');
+                }
+            }
+        }
+
     }
 }
 
